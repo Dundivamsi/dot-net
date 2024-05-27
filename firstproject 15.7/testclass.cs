3971c7bd0dad632b17894d69101c40c9b339d9e2
vamsivarra <121616113+vamsivarra@users.noreply.github.com>
2024-05-27 16:27:09 +0100

    first commit

---
+++ firstproject 15.7/testclass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public void test1()
+        {
+            test2();
+            //Console.WriteLine("csharp");
+        }
+        private void test2()
+        {
+            //Console.WriteLine("python");
+        }
+        public static void Main(string[] args)
+        {
+            Program a123 = new Program();
+            //a123.test1();
+            //a123.test2();
+            Console.WriteLine("Hello World12!");
+        }
+        
+    }
+}
